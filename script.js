5db84d859c75d4efc8173db8e320d060433c951a
ioumar <108413180+ioumar@users.noreply.github.com>
2026-01-12 15:32:51 +0300

    Ajout de la variable globale result, utilisé pour stocker le résulat de l'opération dans la fonction operate. Ajout d'une condition dans la fonction updateNumber et la fonction handleButtonClick. Ajout des fonctions reset et clear pour réinitialiser les variables. Et enfin ajout des événments sur les boutons calculator__btn-equal et calculator__btn-clear.

--- script.js
+++ script.js
@@ -2,34 +2,17 @@ const btnNumeric = Array.from(document.querySelectorAll('.calculator__btn-numeri
 
 const btnOperator = Array.from(document.querySelectorAll('.calculator__btn-operator'));
 
-//const btnEqual = document.querySelector('.calculator__btn-equal');
+const btnEqual = document.querySelector('.calculator__btn-equal');
+const btnClear = document.querySelector('.calculator__btn-clear');
+//const input = document.querySelector('.calculator__input');
+
 
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
-
-
-function operate(firstNumber,secondNumber,operator){
-    switch(operator){
-        case '+':
-            console.log(add(firstNumber, secondNumber));
-            break
-        case '-':
-            console.log(subtract(firstNumber, secondNumber));
-            break;
-        case '*':
-            console.log(multiply(firstNumber, secondNumber));
-            break;
-        case '/':
-            console.log(divide(firstNumber, secondNumber));
-            break;
-        default:
-            console.log('Error');      
-    }
-}
+let result = '';
 
 // Functions for performing basic mathematical operations
-
 
 function add(firstNumber, secondNumber){
     return firstNumber + secondNumber
@@ -53,10 +36,32 @@ function divide(firstNumber, secondNumber){
     return firstNumber / secondNumber;
 }
 
+// function that must return the result of a mathematical operation
 
+function operate(firstNumber,secondNumber,operator){
+    switch(operator){
+        case '+':
+            result = add(firstNumber, secondNumber);
+            console.log(result);
+            break
+        case '-':
+            result = subtract(firstNumber, secondNumber);
+            console.log(result);
+            break;
+        case '*':
+            result = multiply(firstNumber, secondNumber);
+            console.log(result);
+            break;
+        case '/':
+            result = divide(firstNumber, secondNumber);
+            console.log(result);
+            break;
+        default:
+            console.log('Error');      
+    }
+}
 
-// The two functions to update the number and operator
-
+// The three functions to update the number and operator
 
 function handleButtonClick(btnList){
     for(let btn of btnList){
@@ -64,34 +69,60 @@ function handleButtonClick(btnList){
             if (btnNumeric.includes(btn)){
                 updateNumber(event.target.innerText);            
             }
-            else if(btnOperator.includes(btn)){
+            else if(btnOperator.includes(btn) && firstNumber === ''){
                 updateOperator(event.target.innerText);
+                updateNumber(result);
             }
             else{
-                console.log("Error");
+                updateOperator(event.target.innerText);
             }
         });
     }
 }
      
-
 function updateNumber(newNumber){
     if (operator === ''){
         firstNumber = firstNumber + newNumber;
+    }
+    else if(operator !== '' && firstNumber == ''){
+        firstNumber = String(newNumber);
     }
     else{
         secondNumber = secondNumber + newNumber;
     }
 }
 
-
 function updateOperator(newOperator){
     operator = newOperator;
 }
 
+// the two functions to reset the variables
+
+function reset(){
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+}
+
+function clear(){
+    result = '';
+}
+
+//Events to trigger the display of the result and the reset of variables
+
+btnEqual.addEventListener('click',()=>{
+    firstNumber = parseInt(firstNumber);
+    secondNumber = parseInt(secondNumber);
+    operate(firstNumber, secondNumber, operator);
+    reset();
+});
+
+btnClear.addEventListener('click',()=>{
+    clear();
+})
+
+
+
 
 handleButtonClick(btnNumeric);
 handleButtonClick(btnOperator);
-
-
-//operate(firstNumber, secondNumber, operator);
