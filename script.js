fb07089de7a5762b09e9f015b767b63441264aca
ioumar <108413180+ioumar@users.noreply.github.com>
2026-01-12 10:10:54 +0300

    Liaison des fichiers css et js au fichier html. Ajout des fonctions operate, add, subtract, multiply et divide pour la réalisation des opérations mathématiques de base.

---
+++ script.js
@@ -0,0 +1,50 @@
+let firstNumber = parseInt(prompt("Enter your first number : "));
+let operator = prompt('Enter your operator : ')
+let secondNumber = parseInt(prompt("Enter your second number"));
+
+
+function operate(firstNumber,secondNumber,operator){
+    switch(operator){
+        case '+':
+            console.log(add(firstNumber, secondNumber));
+            break
+        case '-':
+            console.log(subtract(firstNumber, secondNumber));
+            break;
+        case '*':
+            console.log(multiply(firstNumber, secondNumber));
+            break;
+        case '/':
+            console.log(divide(firstNumber, secondNumber));
+            break;
+        default:
+            console.log('Erreur');      
+    }
+}
+
+
+function add(firstNumber, secondNumber){
+    return firstNumber + secondNumber
+}
+
+
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+}
+
+
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+}
+
+
+function divide(firstNumber, secondNumber){
+    if(secondNumber === 0){
+        return "Erreur : la division par zéro est impossible";
+    }
+    return firstNumber / secondNumber;
+}
+
+
+
+operate(firstNumber, secondNumber, operator);
